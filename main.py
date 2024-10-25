9f58e67db7f61bfc7358fcba39bf8ed2517e1960
justaweirdo <tube00216@gmail.com>
2024-10-25 19:52:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from cache import cache
 
 max_api_wait_time = 3
 max_time = 10
-apis = ast.literal_eval(requests.get('https://raw.githubusercontent.com/justaweidro/yahoo-inv/main/instances_1.txt').text)
+apis = ast.literal_eval(requests.get('https://raw.githubusercontent.com/justaweidro/yahoo-inv/main/instances.txt').text)
 url = requests.get(r'https://raw.githubusercontent.com/mochidukiyukimi/yuki-youtube-instance/main/instance.txt').text.rstrip()
 version = "1.0"
 
